252d33d81daad8bfbc92e9a3f2e95d5b5322291c
Sven Fuchs <me@svenfuchs.com>
2015-12-16 19:53:52 +0100

    make `push idonethis` a sub-sub command

--- cmd/push.go
+++ cmd/push.go
@@ -2,39 +2,45 @@ package cmd
 
 import (
   "time"
-  "os"
   . "github.com/svenfuchs/todo"
   . "github.com/svenfuchs/todo/service"
 )
 
 
-func NewPushCmd(path string, filter Filter) PushCmd {
+func NewPushCmd(path string, filter Filter, config map[string]string) PushCmd {
   in  := NewFileSource(path)
   out := NewFileSource("")
-  return PushCmd{ Cmd { in, out, filter, "full" } }
+  return PushCmd{ Cmd { in, out, filter, "full" }, config }
 }
 
 type PushCmd struct {
   Cmd
+  config map[string]string
 }
 
 func (c PushCmd) Run() {
-  team    := "personal-2jAM" // os.Getenv("IDONETHIS_TEAM") // TODO
-  user    := os.Getenv("IDONETHIS_USERNAME")
-  token   := os.Getenv("IDONETHIS_TOKEN")
-  after   := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
-  service := NewIdonethis(team, user, token, after)
+  service := c.service(c.config)
   ids, _  := c.ids(service) // TODO report error
 
   list := c.list()
   list = list.Select(c.filter)
   list = list.RejectIf(func(i Item) bool { return includesInt(ids, i.Id) })
 
-  c.Push(list, service)
+  c.push(list, service)
   c.output(list, "full")
 }
 
-func (c PushCmd) Push(list List, service Idonethis) {
+func (c PushCmd) service(config map[string]string) Idonethis {
+  var service Idonethis
+  switch config["service"] {
+    case "idonethis":
+      after := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+      service = NewIdonethis(config["team"], config["username"], config["token"], after)
+  }
+  return service
+}
+
+func (c PushCmd) push(list List, service Idonethis) {
   for _, item := range list.Items {
     service.Push(item.Line) // TODO report error
   }
--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -5,7 +5,6 @@ import (
   "gopkg.in/alecthomas/kingpin.v2"
   . "github.com/svenfuchs/todo"
   . "github.com/svenfuchs/todo/cmd"
-  // "fmt"
 )
 
 type Opts struct {
@@ -17,16 +16,22 @@ type Opts struct {
   text string
   projects []string
   date FilterDate
+  config map[string]string
 }
 
 func (o Opts) filter() Filter {
   return NewFilter(o.id, o.status, o.text, o.projects, o.date)
 }
 
-func (o *Opts) setDate(date string)   { o.date = NewFilterDate("date", date)   }
-func (o *Opts) setBefore(date string) { o.date = NewFilterDate("before", date) }
-func (o *Opts) setSince(date string)  { o.date = NewFilterDate("since", date)  }
-func (o *Opts) setAfter(date string)  { o.date = NewFilterDate("after", date)  }
+func (o *Opts) setDate(date string)     { o.date = NewFilterDate("date", date)   }
+func (o *Opts) setBefore(date string)   { o.date = NewFilterDate("before", date) }
+func (o *Opts) setSince(date string)    { o.date = NewFilterDate("since", date)  }
+func (o *Opts) setAfter(date string)    { o.date = NewFilterDate("after", date)  }
+
+func (o *Opts) setUser(value string)    { o.config["username"] = value }
+func (o *Opts) setToken(value string)   { o.config["token"] = value }
+func (o *Opts) setTeam(value string)    { o.config["team"] = value }
+func (o *Opts) setService(value string) { o.config["service"] = value }
 
 func (o *Opts) runList(c *kingpin.ParseContext) error {
   NewListCmd(o.path, o.filter(), o.format).Run()
@@ -43,51 +48,65 @@ func (o *Opts) runPush(c *kingpin.ParseContext) error {
   if o.date.IsEmpty() {
     o.date = NewFilterDate("yesterday", "since")
   }
-  NewPushCmd(o.path, o.filter()).Run()
+  NewPushCmd(o.path, o.filter(), o.config).Run()
 	return nil
 }
 
 func main() {
-  app := kingpin.New("todo", "A command-line todo.txt tool.")
+  a := kingpin.New("todo", "A command-line todo.txt tool.")
 
 	h := &Opts{}
-	c := app.Command("list", "Filter and list todo items."      ).Action(h.runList)
+	c := a.Command("list", "Filter and list todo items."        ).Action(h.runList)
 	c.Flag("file",     "Todo.txt file to work with."            ).Short('f').StringVar(&h.path)
 	c.Flag("format",   "Output format."                         ).Short('o').StringVar(&h.format)
 	c.Flag("id",       "Filter by id."                          ).Short('i').IntVar(&h.id)
 	c.Flag("status",   "Filter by status."                      ).Short('s').StringVar(&h.status)
 	c.Flag("text",     "Filter by text."                        ).Short('t').StringVar(&h.text)
 	c.Flag("projects", "Filter by projects (comma separated)."  ).Short('p').StringsVar(&h.projects)
-	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue { h.setDate   })
-	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue { h.setAfter  })
-	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue { h.setSince  })
-	c.Flag("before",   "Filter by done before."                 ).Short('b').SetValue(&funcValue { h.setBefore })
+	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue{ h.setDate   })
+	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue{ h.setAfter  })
+	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue{ h.setSince  })
+	c.Flag("before",   "Filter by done before."                 ).Short('b').SetValue(&funcValue{ h.setBefore })
 	c.Arg("input",     "Filter by full line."                   ).StringVar(&h.line)
 
 	h = &Opts{}
-	c = app.Command("toggle", "Toggle todo items."              ).Action(h.runToggle)
+	c = a.Command("toggle", "Toggle todo items."                ).Action(h.runToggle)
 	c.Flag("file",     "Todo.txt file to work with."            ).Short('f').StringVar(&h.path)
 	c.Flag("id",       "Filter by id."                          ).Short('i').IntVar(&h.id)
 	c.Flag("status",   "Filter by status."                      ).Short('s').StringVar(&h.status)
 	c.Flag("text",     "Filter by text."                        ).Short('t').StringVar(&h.text)
 	c.Flag("projects", "Filter by projects (comma separated)."  ).Short('p').StringsVar(&h.projects)
-	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue { h.setDate   })
-	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue { h.setAfter  })
-	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue { h.setSince  })
-	c.Flag("before",   "Filter by done before."                 ).Short('b').SetValue(&funcValue { h.setBefore })
+	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue{ h.setDate   })
+	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue{ h.setAfter  })
+	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue{ h.setSince  })
+	c.Flag("before",   "Filter by done before."                 ).Short('b').SetValue(&funcValue{ h.setBefore })
 	c.Arg("input",     "Filter by full line."                   ).StringVar(&h.line)
 
-	h = &Opts{}
-	c = app.Command("push", "Push todo items."                  ).Action(h.runPush)
+  h = &Opts{ config: map[string]string{} }
+	c = a.Command("push", "Push todo items.")
 	c.Flag("file",     "Todo.txt file to work with."            ).Short('f').StringVar(&h.path)
-	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue { h.setDate  })
-	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue { h.setAfter })
-	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue { h.setSince })
-	c.Arg("input",     "Filter by full line."                   ).StringVar(&h.line)
+	c.Flag("date",     "Filter by done date."                   ).Short('a').SetValue(&funcValue{ h.setDate  })
+	c.Flag("after",    "Filter by done after."                  ).Short('a').SetValue(&funcValue{ h.setAfter })
+	c.Flag("since",    "Filter by done since."                  ).Short('n').SetValue(&funcValue{ h.setSince })
 
-  kingpin.MustParse(app.Parse(os.Args[1:]))
+  s := c.Command("idonethis", "Service Idonethis"             ).Action(h.runPush).PreAction(setFunc(h.setService, "idonethis"))
+  s.Flag("username", "Idonethis username"                     ).Envar("TODO_IDONETHIS_USERNAME").SetValue(&funcValue{ h.setUser })
+  s.Flag("token",    "Idonethis token"                        ).Envar("TODO_IDONETHIS_TOKEN"   ).SetValue(&funcValue{ h.setToken })
+  s.Flag("team",     "Idonethis team"                         ).Envar("TODO_IDONETHIS_TEAM"    ).SetValue(&funcValue{ h.setTeam })
+	s.Arg("input",     "Filter by full line."                   ).StringVar(&h.line)
+
+  kingpin.MustParse(a.Parse(os.Args[1:]))
 }
 
+// For the sub-sub command `todo push [service]` i need to set config["service"]
+// as a PreAction.
+
+func setFunc(f func(string), name string) kingpin.Action {
+  return func(p *kingpin.ParseContext) error {
+    f(name)
+    return nil
+  }
+}
 
 // This uses an internal (?) kingpin api to set values through function
 // callbacks instead of directly forcing them onto a struct field. Not sure if
